30db030f61fccfc1d5781a80f4ecb3fdd8f3df19
Kyd <julien.coche@protonmail.com>
2019-09-03 18:39:44 +0200

    Add requirements and functional tests + remove useless file

---
+++ tests/functional_tests.py
@@ -0,0 +1,9 @@
+#  coding: utf-8
+
+# Connection a la BDD pour recuperer les donnees
+# Ou
+# Ouverture du fichier pour recuperer les donnees
+
+# Tokenization du tweet
+
+# Stockage du resultat dans une DB (Mongo? Peut-être Redis? Un fichier marche aussi)
